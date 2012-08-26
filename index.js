c3438c366240794ccd61affa7e60f753e9ba6e12
Raynos <raynos2@gmail.com>
2012-08-26 16:28:31 -0700

    cleaned up dependencies

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-var invert = require("invert-stream")
-    , uuid = require("node-uuid")
-    , pipeline = require("event-stream").pipeline
-    , through = require("through")
-    , partial = require("ap").partial
-
-var Delta = require("./interfaces/delta")
+var Source = require("source-stream")
+    , Delta = require("./interfaces/delta")
 
 DeltaStream.fromDelta = Delta.from
+DeltaStream.Delta = Delta
 
 module.exports = DeltaStream
 
 function DeltaStream(id) {
-    var inverted = invert()
-        , rejectSource = through(reject)
-        , addSource = through(add)
-        , stream = pipeline(rejectSource, inverted, addSource)
+    var stream = Source()
+        , delta = Delta(stream)
 
-    stream.id = id || uuid()
-    stream.other = inverted.other
     stream.createDelta = returnDelta
 
-    var delta = Delta(stream)
-
     return stream
-
-    function reject(data) {
-        if (data[2] !== stream.id) {
-            this.emit("data", data)
-        }
-    }
-
-    function add(data) {
-        if (!data[2]) {
-            data[2] = stream.id
-        }
-        this.emit("data", data)
-    }
 
     function returnDelta() {
         return delta
