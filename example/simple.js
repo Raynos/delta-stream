724418119f9b63c98595bbf97a036c6e5e6e9b58
Raynos <raynos2@gmail.com>
2012-08-29 22:56:30 -0700

    re-implemented backed by scuttlebutt

--- example/simple.js
+++ example/simple.js
@@ -1,20 +1,53 @@
-var DeltaStream = require("../index")
+var Delta = require("../index")
+    , json = require("stream-serializer").json
+    , net = require("net")
+    , log = require("event-stream").log
 
-var stream1 = DeltaStream()
-    , stream2 = DeltaStream()
+var server = net.createServer(function listen(stream) {
+    var delta = Delta()
+        , networkStream = delta.createStream()
 
-var delta1 = stream1.createDelta()
-    , delta2 = stream2.createDelta()
+    delta.on("change:local", function (value) {
+        console.log("[CHANGE:LOCAL]", value)
+    })
 
-delta1.on("change:other", function (changes) {
-    console.log("[CHANGE:OTHER]", changes)
+    delta.set("remote", "local")
+
+    stream.pipe(json(networkStream)).pipe(stream)
+
+    setTimeout(function () {
+        stream.end()
+        server.close()
+    }, 500)
+}).listen(12503, function connect() {
+    var stream = net.connect(12503)
+
+    var delta1 = Delta()
+        , delta2 = Delta()
+
+    var stream1 = delta1.createStream()
+        , stream2 = delta2.createStream()
+
+    stream1.pipe(stream2).pipe(stream1)
+
+    delta1.on("change:delta1", function (value) {
+        console.log("[CHANGE:DELTA1]", value)
+    })
+
+    delta2.on("change:delta2", function (value) {
+        console.log("[CHANGE:DELTA2]", value)
+    })
+
+    delta1.set("delta2", "hello")
+    delta2.set("delta1", "world")
+
+    delta1.on("change:remote", function (value) {
+        console.log("[CHANGE:REMOTE]", value)
+    })
+
+    delta1.set("local", "remote")
+
+    // This is scuttlebutt stream
+    var networkStream = delta1.createStream()
+    stream.pipe(json(networkStream)).pipe(stream)
 })
-
-delta2.on("change:foo", function (value) {
-    console.log("[CHANGE:FOO]", value)
-})
-
-stream2.pipe(stream1).pipe(stream2)
-
-delta1.set("foo", "bar")
-delta2.set("other", "thing")
--- index.js
+++ index.js
@@ -1,32 +1,86 @@
-var Source = require("source-stream")
-    , Delta = require("./interfaces/delta")
+var Scuttlebutt = require("scuttlebutt")
+    , updateIsRecent = Scuttlebutt.updateIsRecent
+    , reduce = require("iterators").reduceSync
 
-DeltaStream.fromDelta = Delta.createStream
-DeltaStream.Delta = Delta
+module.exports = Delta
 
-module.exports = DeltaStream
+/*
+    Protocol is [key, value, source, timestamp]
+*/
+function Delta() {
+    var stream = Scuttlebutt()
+        , localUpdate = stream.localUpdate.bind(stream)
+        , updates = {}
+        , state = {}
 
-function DeltaStream(id) {
-    var stream = Source()
-        , delta
-
-    stream.createDelta = returnDelta
-    stream.sync = sync
+    stream.applyUpdate = applyUpdate
+    // history is a global variable -.-
+    stream.history = $history
+    stream.set = localUpdate
+    stream.get = get
+    stream.has = has
+    // stupid tokens
+    stream.delete = $delete
+    stream.toJSON = toJSON
 
     return stream
 
-    function returnDelta() {
-        if (!delta) {
-            delta = Delta(stream)
+    function applyUpdate(update) {
+        var key = update[0]
+            , value = update[1]
+            , recentUpdate = updates[key]
+
+        // If the most recent update for that key is newer then the incoming
+        // update. Then ignore it
+        if (recentUpdate && recentUpdate[3] > update[3]) {
+            return
         }
 
-        return delta
+        updates[key] = update
+
+        if (value === undefined) {
+            delete state[key]
+        } else {
+            state[key] = value
+        }
+
+        stream.emit("change", key, value)
+        stream.emit("change:" + key, value)
+
+        return true
     }
 
-    function sync(other) {
-        var selfDelta = returnDelta()
-            , otherDelta = other.createDelta()
-
-        selfDelta.sync(otherDelta)
+    function $history(sources) {
+        return reduce(updates, isRecent, sources, []).sort(byTimestamp)
     }
+
+    function get(key) {
+        return state[key]
+    }
+
+    function has(key) {
+        return key in state
+    }
+
+    function $delete(key) {
+        localUpdate(key, undefined)
+    }
+
+    function toJSON() {
+        return state
+    }
+}
+
+function isRecent(acc, update) {
+    var sources = this
+
+    if (updateIsRecent(update, sources)) {
+        acc.push(update)
+    }
+
+    return acc
+}
+
+function byTimestamp(a, b) {
+    return a[3] - b[3]
 }
